caf2e9723afa9aa351f12051b30afd58bf128428
Asankamb <47291721+Asankamb@users.noreply.github.com>
2022-03-13 13:09:29 +0530

    first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String args[]){
+        print();
+        System.out.println(add(4,5));
+    }
+
+    public static void print(){
+
+        System.out.println("Hello world");
+    }
+    public static int add(int num1,int num2){
+        return num1+num2;
+    }
+}
